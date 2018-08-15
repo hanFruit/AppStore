87e5ddfd369e402cb9b3964a07704ca3886114b3
韩志尧 <hzyshowtime@foxmail.com>
2018-08-15 09:05:01 +0800

    Initial commit

---
+++ app/src/main/java/com/chain/pro/appstore/MainActivity.java
@@ -0,0 +1,95 @@
+package com.chain.pro.appstore;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import com.chain.pro.appstore.adapter.FragmentAdapter;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.container)
+    LinearLayout container;
+    @BindView(R.id.drawer_layout)
+    DrawerLayout drawerLayout;
+    @BindView(R.id.navigation_view)
+    NavigationView navigationView;
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+    @BindView(R.id.tablayout)
+    TabLayout tablayout;
+    @BindView(R.id.viewpager)
+    ViewPager viewpager;
+    private View headerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+
+
+        headerView = navigationView.getHeaderView(0);
+        headerView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "头点击事件", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+                switch (menuItem.getItemId()) {
+
+                    case R.id.menu_app_update:
+                        Toast.makeText(MainActivity.this, "应用更新", Toast.LENGTH_SHORT).show();
+
+                        break;
+                    case R.id.menu_download_manager:
+                        Toast.makeText(MainActivity.this, "下载管理", Toast.LENGTH_SHORT).show();
+
+                        break;
+                    case R.id.menu_app_uninstall:
+                        Toast.makeText(MainActivity.this, "卸载", Toast.LENGTH_SHORT).show();
+
+                        break;
+                    case R.id.menu_setting:
+                        Toast.makeText(MainActivity.this, "设置", Toast.LENGTH_SHORT).show();
+
+                        break;
+
+                }
+                return false;
+            }
+        });
+
+
+        toolbar.inflateMenu(R.menu.toolbar_menu);
+        ActionBarDrawerToggle drawerToggle = new ActionBarDrawerToggle(MainActivity.this, drawerLayout, toolbar, R.string.open, R.string.close);
+        drawerToggle.syncState();
+        drawerLayout.addDrawerListener(drawerToggle);
+
+
+        FragmentAdapter adapter = new FragmentAdapter(getSupportFragmentManager());
+        viewpager.setAdapter(adapter);
+        tablayout.setupWithViewPager(viewpager);
+
+    }
+}
---
+++ app/src/main/java/com/chain/pro/appstore/adapter/FragmentAdapter.java
@@ -0,0 +1,55 @@
+package com.chain.pro.appstore.adapter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.view.ViewGroup;
+
+import com.chain.pro.appstore.bean.FragmentInfo;
+import com.chain.pro.appstore.fragment.CategoryFragment;
+import com.chain.pro.appstore.fragment.GameFragment;
+import com.chain.pro.appstore.fragment.RankFragment;
+import com.chain.pro.appstore.fragment.RecommandFragment;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FragmentAdapter extends FragmentStatePagerAdapter {
+
+    private List<FragmentInfo> list = new ArrayList<>();
+
+    public FragmentAdapter(FragmentManager fm) {
+        super(fm);
+
+        initFragments();
+    }
+
+    private void initFragments() {
+        list.add(new FragmentInfo("推荐", RecommandFragment.class));
+        list.add(new FragmentInfo("排行", RankFragment.class));
+        list.add(new FragmentInfo("游戏", GameFragment.class));
+        list.add(new FragmentInfo("分类", CategoryFragment.class));
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        try {
+            return (Fragment) list.get(position).getFragment().newInstance();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return list.size();
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return list.get(position).getTitle();
+    }
+}
